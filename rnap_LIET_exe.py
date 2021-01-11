658fa6c78212e00ab7ecfe822befe9627f5ce95c
Jacob Stanley <jtstan@gmail.com>
2021-01-11 16:55:44 -0700

    Put main loop in try/except to avoid fit errors temporarily. Also, added results file initializer.

--- rnap_LIET_exe.py
+++ rnap_LIET_exe.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 
 from rnap_lib_LIET_class import LIET
@@ -15,95 +16,119 @@ config = dp.config_loader(config_file)
 annot_file = config['FILES']['ANNOTATION']
 annot_dict = dp.annot_loader(annot_file)
 
-# Open results and log files
+# Open results and log files and initialize them
+res_file = open(config['FILES']['RESULTS'], 'w')
+fr.res_file_init(res_file, config_file)                                      # Need to finalize this function
 
 # Open bedgraph file
 bg_file = config['FILES']['BEDGRAPH']
-with open(bg_file, 'r') as bg:
+try:
+    with open(bg_file, 'r') as bg:
 
-## MAIN LOOP OVER ANNOTATIONS =================================================
-    for chromosome, annotations in annot_dict.items():
+    ## MAIN LOOP OVER ANNOTATIONS =================================================
+        for chromosome, annotations in annot_dict.items():
 
-        for region, gene_id in annotations.items():
-            
-            start = region[0]
-            stop = region[1]
-            strand = region[2]
+            for region, gene_id in annotations.items():
+                
+                start = region[0]
+                stop = region[1]
+                strand = region[2]
 
-            # Determine pad
-            pad = config['DATA_PROC']['PAD']
-            if isinstance(pad, float):
-                pad = round((stop - start) * pad)
+                # Determine pad
+                pad = config['DATA_PROC']['PAD']
+                if isinstance(pad, float):
+                    pad = round((stop - start) * pad)
 
-            # Extract reads from bedgraph file for region
-            begin = start - pad
-            end = stop + pad
-            current_line, preads, nreads = dp.bgreads(bg, begin, end)
+                # Extract reads from bedgraph file for region
+                begin = start - pad
+                end = stop + pad
+                current_line, preads, nreads = dp.bgreads(bg, begin, end)
 
-            # Convert reads to list
-            pread_list = []
-            nread_list = []
-            for position, count in preads.items():
-                pread_list.extend([position]*count)
-            for position, count in nreads.items():
-                nread_list.extend([position]*count)
+                # Convert reads to list
+                pread_list = []
+                nread_list = []
+                for position, count in preads.items():
+                    pread_list.extend([position]*count)
+                for position, count in nreads.items():
+                    nread_list.extend([position]*count)
 
-            # Generate model object
-            liet = LIET()
+                # Generate model object
+                liet = LIET()
 
-            # Load annotation into LIET class object
-            annot = {
-                'gene_id': gene_id, 
-                'chrom': chromosome, 
-                'start': start, 
-                'stop': stop, 
-                'strand': strand
-            }
-            liet.load_annotation(**annot)
+                # Load annotation into LIET class object
+                annot = {
+                    'gene_id': gene_id, 
+                    'chrom': chromosome, 
+                    'start': start, 
+                    'stop': stop, 
+                    'strand': strand
+                }
+                liet.load_annotation(**annot)
 
-            # Load read data into LIET class object
-            coordinates = list(range(begin, end))
-            data = {
-                'positions': coordinates, 
-                'pos_reads': pread_list, 
-                'neg_reads': nread_list, 
-                'pad': pad, 
-                'shift': config['DATA_PROC']['RANGE_SHIFT']
-            }
-            liet.load_seq_data(**data)
+                # Load read data into LIET class object
+                coordinates = list(range(begin, end))
+                data = {
+                    'positions': coordinates, 
+                    'pos_reads': pread_list, 
+                    'neg_reads': nread_list, 
+                    'pad': pad, 
+                    'shift': config['DATA_PROC']['RANGE_SHIFT']
+                }
+                liet.load_seq_data(**data)
 
-            # Load priors
-            if config['DATA_PROC']['RANGE_SHIFT']:
-                tss = 0
-                tts = abs(stop-start)
-            else:
-                tss = start
-                tts = stop
-            priors = dp.prior_config(config['PRIORS'], tss, tts)
+                # Load priors
+                if config['DATA_PROC']['RANGE_SHIFT']:
+                    tss = 0
+                    tts = abs(stop-start)
+                else:
+                    tss = start
+                    tts = stop
+                priors = dp.prior_config(config['PRIORS'], tss, tts)
 
-            liet.set_priors(**priors)
+                liet.set_priors(**priors)
 
-            # Build model
-            liet.build_model(
-                antisense=config['MODEL']['ANTISENSE'],
-                background=config['MODEL']['BACKGROUND']
-            )
+                # Build model
+                liet.build_model(
+                    antisense=config['MODEL']['ANTISENSE'],
+                    background=config['MODEL']['BACKGROUND']
+                )
 
-            # Fit
-            fit = fr.vi_fit(
-                liet.model,
-                method=config['FIT']['METHOD'],
-                optimizer=config['FIT']['OPTIMIZER'],
-                learning_rate=config['FIT']['LEARNING_RATE'],
-                start=None,                                                 # NEEDS IMPLEMENTATION
-                iterations=config['FIT']['ITERATIONS'],
-                tolerance=config['FIT']['TOLERANCE'],                       # NEED TO FIX IMPLEMENTATION
-                param_tracker=False,                                        # NEEDS IMPLEMENTATION
-            )
-            
-# Summarize posteriors
-# Evaluate "best fit" values
-# Evaluate whether or not to refit
+                # Fit
+                fit = fr.vi_fit(
+                    liet.model,
+                    method=config['FIT']['METHOD'],
+                    optimizer=config['FIT']['OPTIMIZER'],
+                    learning_rate=config['FIT']['LEARNING_RATE'],
+                    start=None,                                                 # NEEDS IMPLEMENTATION
+                    iterations=config['FIT']['ITERATIONS'],
+                    tolerance=config['FIT']['TOLERANCE'],                       # NEED TO FIX IMPLEMENTATION
+                    param_tracker=False,                                        # NEEDS IMPLEMENTATION
+                )
 
-# Refit OR record results to results file
+    ## Currently omitting these steps =============================================
+    # Evaluate "best fit" values
+    # Evaluate whether or not to refit 
+    # Run refit if convergence criteria not met
+    ## ============================================================================
 
+                # Summarize posteriors
+                post_stats = fr.posterior_stats(
+                    fit['approx'],
+                    N=config['RESULTS']['SAMPLES'],
+                    mean=config['RESULTS']['MEAN'],
+                    mode=config['RESULTS']['MODE'],
+                    median=config['RESULTS']['MEDIAN'],
+                    stdev=config['RESULTS']['STDEV'],
+                    skew=config['RESULTS']['SKEW']
+                )
+
+                # Record results of fitting
+                fr.results_record()
+
+except:
+    res_file.write("RUN ERROR")
+    res_file.close()
+    sys.exit(1)
+    
+res_file.close()
+sys.exit(0)
