b5bec0ad89b42d00896fc28e8c524758ab814fe2
Jacob Stanley <jtstan@gmail.com>
2021-03-26 11:53:02 -0600

    Added logging

--- rnap_LIET_exe.py
+++ rnap_LIET_exe.py
@@ -9,49 +9,53 @@ import rnap_lib_fitting_results as fr
 ## PARSE INPUT FILES ==========================================================
 
 # Load config (contents: FILES, MODEL, PRIORS, DATA_PROC, FITTING, RESULTS)
-config_file = "C:\\Users\\Jacob\\Dropbox\\0DOWELL\\rnap_model\\LIET_test\\LIET_test_config.txt"
+config_file = "C:\\Users\\Jacob\\Dropbox\\0DOWELL\\rnap_model\\LIET\\test_config.txt"
 config = dp.config_loader(config_file)
 
 # Parse annotation file
 annot_file = config['FILES']['ANNOTATION']
 annot_dict = dp.annot_loader(annot_file)
 
+for k, v in annot_dict.items():
+    print(k, '\t', v)
+    
 # Open results and log files and initialize them
-res_file = open(config['FILES']['RESULTS'], 'w')
-fr.res_file_init(res_file, config_file)                                      # Need to finalize this function
+res_filename = config['FILES']['RESULTS']
+res_file = open(res_filename, 'w')
+fr.res_file_init(res_file, config_file) 
+
+log_filename = f"{res_filename}.log"                                     # Need to finalize this function
+log_file = open(log_filename, 'w')
+fr.log_file_init(log_file)
 
 # Open bedgraph file
 bg_file = config['FILES']['BEDGRAPH']
-try:
-    with open(bg_file, 'r') as bg:
 
-        ## MAIN LOOP OVER ANNOTATIONS =========================================
-        for chromosome, annotations in annot_dict.items():
+with open(bg_file, 'r') as bg:
+    current_bgl = ['chr1', 0, 0, 0]    # Initialize current line
+    ## MAIN LOOP OVER ANNOTATIONS =========================================
+    for chromosome, annotations in annot_dict.items():
+        print(chromosome)
+        for region, gene_id in annotations.items():
+            
+            print(f'Running: {gene_id}')
+            start, stop, strand = region
+            
+            # Compute padded range
+            pad = config['DATA_PROC']['PAD']
+            pad_args = [start, stop, strand, pad]
+            adj_start, adj_stop = dp.pad_calc(*pad_args)
+            # Extract reads
+            bg_args = [bg, current_bgl, chromosome, adj_start, adj_stop]
+            print(f"BG_ARGS: {bg_args[1:]}")
+            current_bgl, preads, nreads = dp.bgreads(*bg_args)
+            # Convert read dict to list
+            preads_list = np.array(dp.reads_d2l(preads))
+            nreads_list = np.array(dp.reads_d2l(nreads))
 
-            for region, gene_id in annotations.items():
-                
-                start = region[0]
-                stop = region[1]
-                strand = region[2]
+            print(f"Range: {adj_start}, {adj_stop}, {strand}")
 
-                # Determine pad
-                pad = config['DATA_PROC']['PAD']
-                if isinstance(pad, float):
-                    pad = round((stop - start) * pad)
-
-                # Extract reads from bedgraph file for region
-                begin = start - pad
-                end = stop + pad
-                current_line, preads, nreads = dp.bgreads(bg, begin, end)
-
-                # Convert reads to list
-                pread_list = []
-                nread_list = []
-                for position, count in preads.items():
-                    pread_list.extend([position]*count)
-                for position, count in nreads.items():
-                    nread_list.extend([position]*count)
-
+            try:
                 # Generate model object
                 liet = LIET()
 
@@ -64,35 +68,44 @@ try:
                     'strand': strand
                 }
                 liet.load_annotation(**annot)
-
+                print(annot)
+            except:
+                print("Error load annotation")
+            try:
                 # Load read data into LIET class object
-                coordinates = list(range(begin, end))
+                coordinates = np.array(range(adj_start, adj_stop))
                 data = {
                     'positions': coordinates, 
-                    'pos_reads': pread_list, 
-                    'neg_reads': nread_list, 
+                    'pos_reads': preads_list, 
+                    'neg_reads': nreads_list, 
                     'pad': pad, 
                     'shift': config['DATA_PROC']['RANGE_SHIFT']
                 }
                 liet.load_seq_data(**data)
-
+            except:
+                print("Error loading data")
+                print("PAD: ", pad)
+                print(f"READS: {preads_list[0:10]}, {nreads_list[0:10]}")
+            try:
                 # Load priors
                 if config['DATA_PROC']['RANGE_SHIFT']:
                     tss = 0
-                    tts = abs(stop-start)
+                    tcs = abs(stop-start)
                 else:
                     tss = start
-                    tts = stop
-                priors = dp.prior_config(config['PRIORS'], tss, tts)
+                    tcs = stop
+                priors = dp.prior_config(config['PRIORS'], tss, tcs)
 
                 liet.set_priors(**priors)
-
+            except:
+                print("Error setting priors")
+            try:
                 # Build model
                 liet.build_model(
                     antisense=config['MODEL']['ANTISENSE'],
                     background=config['MODEL']['BACKGROUND']
                 )
-
+                print('Running fit...')
                 # Fit
                 fit = fr.vi_fit(
                     liet.model,
@@ -105,31 +118,35 @@ try:
                     param_tracker=False,                                        # NEEDS IMPLEMENTATION
                 )
 
-                ## Currently omitting these steps =============================
-                # Evaluate "best fit" values
-                # Evaluate whether or not to refit 
-                # Run refit if convergence criteria not met
-                ## ============================================================
-
+            ## Currently omitting these steps =============================
+            # Evaluate "best fit" values
+            # Evaluate whether or not to refit 
+            # Run refit if convergence criteria not met
+            ## ============================================================
+                print("Summarizing post...")
                 # Summarize posteriors
                 post_stats = fr.posterior_stats(
                     fit['approx'],
                     N=config['RESULTS']['SAMPLES'],
-                    mean=config['RESULTS']['MEAN'],
-                    mode=config['RESULTS']['MODE'],
-                    median=config['RESULTS']['MEDIAN'],
-                    stdev=config['RESULTS']['STDEV'],
-                    skew=config['RESULTS']['SKEW']
+                    calc_mean=config['RESULTS']['MEAN'],
+                    calc_mode=config['RESULTS']['MODE'],
+                    calc_median=config['RESULTS']['MEDIAN'],
+                    calc_stdev=config['RESULTS']['STDEV'],
+                    calc_skew=config['RESULTS']['SKEW']
                 )
 
                 # Record results of fitting
                 res = fr.results_format(gene_id, post_stats, stat='mean')
                 res_file.write(res)
 
-except:
-    res_file.write("RUN ERROR")
-    res_file.close()
-    sys.exit(1)
+                fr.log_write(log_file, liet, fit)
+            except:
+                res_file.write(f"{gene_id}\t{start}\t{stop}\t{strand}\tERROR\n")
+#except:
+#    res_file.write("RUN ERROR")
+#    res_file.close()
+#    sys.exit(1)
     
 res_file.close()
+log_file.close()
 sys.exit(0)
