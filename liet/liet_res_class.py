b1f95c9c4971499cd026eeb56d910c21a8aa8201
jtstanley <jtstan@gmail.com>
2022-07-31 21:41:52 -0600

    Debugging previous commit

--- liet/liet_res_class.py
+++ liet/liet_res_class.py
@@ -110,7 +110,6 @@ class FitParse:
                         gene_id = line[0][1:]
                         self.log[gene_id] = dict()
                     else:
-                        print(line)
                         field, value = line.strip().split(':')
                         if field == 'strand_cov':
                             value = value.strip('()').split(',')
